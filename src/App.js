9f6b573de4d94efaa0b303334cd46e196e921391
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-14 15:01:46 -0700

    signup and sign in working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+    Link,
+} from "react-router-dom";
+// import Todos from './Todos.js';
+import SignUp from './SignUp.js'
+import SignIn from './SignIn.js'
+// import PrivateRoute from './PrivateRoute.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { token: localStorage.getItem('TOKEN') }
+
+  handleTokenChange = (myToken) => {
+    this.setState({ token: myToken });
+    localStorage.setItem('TOKEN', myToken);
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <ul>
+            { this.state.token && <div>Welcome, user!!!</div> }
+            { this.state.token && <Link to="/todos"><div>Todos</div></Link> }
+            <Link to="/signin"><div>Sign in</div></Link>
+            <Link to="/signup"><div>Sign up</div></Link>
+            <button onClick={() =>this.handleTokenChange('')}>Logout</button>
+          </ul>
+          <Switch>
+            <Route exact path='/signin' render={(routerProps) => <SignIn 
+                handleTokenChange={this.handleTokenChange} 
+                {...routerProps} />} 
+              />
+            <Route 
+            exact path='/signup' 
+              render={(routerProps) => <SignUp 
+                handleTokenChange={this.handleTokenChange} 
+                {...routerProps}/>} 
+              />
+            {/* <PrivateRoute 
+              exact 
+              path='/todos' 
+              token={this.state.token} 
+              render={(routerProps) => <Todos 
+              {...routerProps} />} /> */}
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/SignUp.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import request from 'superagent';
+
+export default class SignUp extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const data = await request.post('http://localhost:3001/auth/signup', this.state)
+        console.log(data);
+
+        this.props.handleTokenChange(data.body.token);
+        this.props.history.push('/todos');
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <h1>Sign up</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email 
+                        <input type='text' onChange={this.handleChange} name='email'></input>
+                    </label>
+                    <label>
+                        Password
+                        <input type='text' onChange={this.handleChange} name='password'></input>
+                    </label>
+                    <input type='submit'></input>
+                </form>
+
+                
+            </div>
+        )
+    }
+}
